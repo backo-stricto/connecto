471878b9e3cc40a6fa32553576aca73c768cada6
Paul Breugnot <paul.breugnot@inria.fr>
2026-06-18 16:44:23 +0200

    feat: backo.utils.nested_data_path utility algorithms

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,3 +3,8 @@ all imports
 """
 
 from .test_database import TestDatabaseEngine, TestDatabaseItem
+from .test_utils import (
+    TestNestedDataPathFind,
+    TestNestedDataPathUpdate,
+    TestNestedDataPathDelete,
+)
