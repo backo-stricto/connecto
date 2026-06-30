bwallrich/backo

feat(#8) LDAP connector and items (search)
Implements database components required to perform a get_by_id on an
LDAP database.

backo/ldap/connection.py
@@ -1,0 +1,92 @@
+import ldap3
+
+from typing import Any
+from backo.error import NotFoundError
+from .request import LdapSearchRequest
+
+
+class LdapConnection:
+    def __init__(self, ldap_connection: ldap3.Connection):
+        """Creates a new LdapConnection.
+
+        See the [ldap3
+        documention](https://ldap3.readthedocs.io/en/latest/connection.html#)
+        for how to init the LDAP connection. Common examples:
+        - LDAP connection without TLS
+          ```python
+          ldap_server = ldap3.Server("ldap://ldap.example.org")
+          ldap_connection = ldap3.Connection(
+              ldap_server,
+              "username",
+              "password",
+              auto_bind=True
+          )
+          ```
+        - LDAP connection with TLS
+          ```python
+          ldap_server = ldap3.Server(
+              "ldaps://ldap.example.org",
+              use_ssl=True
+            )
+          ldap_connection = ldap3.Connection(
+              ldap_server,
+              "username",
+              "password",
+              auto_bind=True
+          )
+          ```
+        - LDAP connection with TLS and a custom CA
+          ```python
+          ldap_server = ldap3.Server(
+            "ldaps://ldap.example.org",
+            use_ssl=True,
+            tls=ldap3.Tls(ca_certs_file="/path/to/custom/ca.crt")
+          )
+          ldap_connection = ldap3.Connection(
+              ldap_server,
+              "username",
+              "password",
+              auto_bind=True
+          )
+          ```
+
+        :param ldap_connection: An ldap3.Connection instance used to perform
+        LDAP operations
+        """
+        self.ldap_connection = ldap_connection
+
+    def execute_search(
+        self, ldap_search_request: LdapSearchRequest
+    ) -> list[dict[str, Any]]:
+        """Performs an LDAP SEARCH operation with parameters of the
+        ldap_search_request.
+
+        The response is formatted as specified in the [ldap3
+        documentation](https://ldap3.readthedocs.io/en/latest/searches.html#response).
+        It is a list of LDAP entries, each of which is represented by a dict
+        containing "dn" and "attributes" keys. The value associated to
+        "attributes" is itself a dict mapping LDAP attributes name to their
+        values, formatted according to the current LDAP schemas.
+
+        :param ldap_search_request: LDAP search parameters
+        """
+
+        self.ldap_connection.search(
+            ldap_search_request.search_base,
+            ldap_search_request.search_filter,
+            attributes=ldap_search_request.attributes,
+        )
+
+        ldap_response = []
+        try:
+            ldap_response = self.ldap_connection.response
+        except IndexError as e:
+            # Occurs if response is empty
+
+            raise NotFoundError(
+                'Ldap search "{0}" with filter {1} did not returned any result.',
+                ldap_search_request.search_base,
+                ldap_search_request.search_filter,
+            ) from e
+
+        return ldap_response

tests/test_ldap/test_item.py
@@ -1,0 +1,165 @@
+import unittest
+
+from hamcrest import (
+    assert_that,
+    contains_exactly,
+    has_entries,
+    contains_inanyorder,
+)
+
+from backo.database.item import DatabaseItem
+
+from backo.ldap.item import (
+    MapDNToBase64ID,
+    MapAttributeToID,
+    Attribute,
+    LdapRef,
+    RefAttributeToBase64ID,
+)
+
+
+class TestLdapItem(unittest.TestCase):
+    def test_load_attribute(self):
+        """Tests LdapItem.load method for a simple item, without references.
+
+        The item must be loaded from the mocked LDAP response.
+        """
+
+        id_mapper = MapDNToBase64ID()
+        ldap_item = DatabaseItem(
+            id_mapper,
+            {
+                "login": Attribute("uid"),
+                "name": Attribute("name"),
+                "contact": Attribute("mail"),
+            },
+        )
+
+        dn = "uid=jdoe,ou=people,dc=example,dc=org"
+
+        # Real call to the method under test
+        item = ldap_item.load(
+            [
+                {
+                    "dn": dn,
+                    "attributes": {
+                        # Single values
+                        "uid": "jdoe",
+                        "uidNumber": 12,
+                        "name": "John Doe",
+                        # A true multiple value
+                        "mail": ["mail1@example.org", "mail2@jdoe.fr"],
+                    },
+                }
+            ],
+            {
+                "login": None,
+                "name": None,
+                "contact": None,
+            },
+        )
+
+        assert_that(
+            item,
+            has_entries(
+                {
+                    "login": "jdoe",
+                    "name": "John Doe",
+                    "contact": contains_exactly("mail1@example.org", "mail2@jdoe.fr"),
+                }
+            ),
+        )
+
+    def test_load_simple_ref(self):
+        """Tests LdapItem.load method for a simple item, without references.
+
+        The item must be loaded from the mocked LDAP response.
+        """
+
+        id_mapper = MapDNToBase64ID()
+        ldap_item = DatabaseItem(
+            id_mapper,
+            {
+                # No reverse is specified in the LdapRef specification, so it is
+                # assumed that the value itself is alread a backo _id. It does
+                # not need to be transformed.
+                "primary_group": LdapRef(attribute="gidNumber")
+            },
+        )
+
+        dn = "uid=jdoe,ou=people,dc=example,dc=org"
+
+        # Real call to the method under test
+        item = ldap_item.load(
+            [
+                {
+                    "dn": dn,
+                    "attributes": {
+                        "gidNumber": 1312,
+                    },
+                }
+            ],
+            {"primary_group": None},
+        )
+
+        assert_that(
+            item,
+            has_entries(
+                {
+                    "primary_group": 1312,
+                }
+            ),
+        )
+
+        assert_that(item.keys(), contains_exactly("primary_group"))
+
+    def test_load_dn_ref(self):
+        """Tests LdapItem.load method for a simple item, without references.
+
+        The item must be loaded from the mocked LDAP response.
+        """
+
+        # Example id mapper used by the LdapItem
+        group_id_mapper = MapAttributeToID("ou=groups,dc=example,dc=org", "gidNumber")
+
+        # Id mapper used by the superadmin reference
+
+        ldap_group_item = DatabaseItem(
+            group_id_mapper,
+            {
+                # Backo field
+                "superadmin": LdapRef(
+                    # LDAP attribute
+                    attribute="owner",
+                    # The owner attribute is a DN that must be converted to a
+                    # valid backo _id
+                    reverse=RefAttributeToBase64ID(),
+                )
+            },
+        )
+
+        dn = "uid=jdoe,ou=people,dc=example,dc=org"
+
+        # Real call to the method under test
+        item = ldap_group_item.load(
+            [
+                {
+                    "dn": dn,
+                    "attributes": {
+                        "owner": "uid=jdoe,ou=people,dc=example,dc=org",
+                    },
+                }
+            ],
+            {"superadmin": None},
+        )
+
+        assert_that(
+            item,
+            has_entries(
+                {
+                    "superadmin": RefAttributeToBase64ID().ref_id(
+                        "uid=jdoe,ou=people,dc=example,dc=org"
+                    ),
+                }
+            ),
+        )

backo-stricto/connecto

src/connecto/ldap/connection.py
@@ -1,0 +1,91 @@
+import ldap3
+
+from typing import Any
+from ..error import ItemNotFound
+from .request import LdapSearchRequest
+
+
+class LdapConnection:
+    def __init__(self, ldap_connection: ldap3.Connection):
+        """Creates a new LdapConnection.
+
+        See the [ldap3
+        documention](https://ldap3.readthedocs.io/en/latest/connection.html#)
+        for how to init the LDAP connection. Common examples:
+        - LDAP connection without TLS
+          ```python
+          ldap_server = ldap3.Server("ldap://ldap.example.org")
+          ldap_connection = ldap3.Connection(
+              ldap_server,
+              "username",
+              "password",
+              auto_bind=True
+          )
+          ```
+        - LDAP connection with TLS
+          ```python
+          ldap_server = ldap3.Server(
+              "ldaps://ldap.example.org",
+              use_ssl=True
+            )
+          ldap_connection = ldap3.Connection(
+              ldap_server,
+              "username",
+              "password",
+              auto_bind=True
+          )
+          ```
+        - LDAP connection with TLS and a custom CA
+          ```python
+          ldap_server = ldap3.Server(
+            "ldaps://ldap.example.org",
+            use_ssl=True,
+            tls=ldap3.Tls(ca_certs_file="/path/to/custom/ca.crt")
+          )
+          ldap_connection = ldap3.Connection(
+              ldap_server,
+              "username",
+              "password",
+              auto_bind=True
+          )
+          ```
+
+        :param ldap_connection: An ldap3.Connection instance used to perform
+        LDAP operations
+        """
+        self.ldap_connection = ldap_connection
+
+    def execute_search(
+        self, ldap_search_request: LdapSearchRequest
+    ) -> list[dict[str, Any]]:
+        """Performs an LDAP SEARCH operation with parameters of the
+        ldap_search_request.
+
+        The response is formatted as specified in the [ldap3
+        documentation](https://ldap3.readthedocs.io/en/latest/searches.html#response).
+        It is a list of LDAP entries, each of which is represented by a dict
+        containing "dn" and "attributes" keys. The value associated to
+        "attributes" is itself a dict mapping LDAP attributes name to their
+        values, formatted according to the current LDAP schemas.
+
+        :param ldap_search_request: LDAP search parameters
+        """
+
+        self.ldap_connection.search(
+            ldap_search_request.search_base,
+            ldap_search_request.search_filter,
+            attributes=ldap_search_request.attributes,
+        )
+
+        ldap_response = []
+        try:
+            ldap_response = self.ldap_connection.response
+        except IndexError as e:
+            # Occurs if response is empty
+
+            raise ItemNotFound(
+                    f"{ldap_search_request} with filter {ldap_search_request.search_filter}",
+                    self.ldap_connection
+                    ) from e
+
+        return ldap_response

tests/test_connecto/test_ldap/test_item.py
@@ -1,0 +1,165 @@
+import unittest
+
+from hamcrest import (
+    assert_that,
+    contains_exactly,
+    has_entries,
+    contains_inanyorder,
+)
+
+from connecto.item import DatabaseItem
+
+from connecto.ldap.item import (
+    MapDNToBase64ID,
+    MapAttributeToID,
+    Attribute,
+    LdapRef,
+    RefAttributeToBase64ID,
+)
+
+
+class TestLdapItem(unittest.TestCase):
+    def test_load_attribute(self):
+        """Tests LdapItem.load method for a simple item, without references.
+
+        The item must be loaded from the mocked LDAP response.
+        """
+
+        id_mapper = MapDNToBase64ID()
+        ldap_item = DatabaseItem(
+            id_mapper,
+            {
+                "login": Attribute("uid"),
+                "name": Attribute("name"),
+                "contact": Attribute("mail"),
+            },
+        )
+
+        dn = "uid=jdoe,ou=people,dc=example,dc=org"
+
+        # Real call to the method under test
+        item = ldap_item.load(
+            [
+                {
+                    "dn": dn,
+                    "attributes": {
+                        # Single values
+                        "uid": "jdoe",
+                        "uidNumber": 12,
+                        "name": "John Doe",
+                        # A true multiple value
+                        "mail": ["mail1@example.org", "mail2@jdoe.fr"],
+                    },
+                }
+            ],
+            {
+                "login": None,
+                "name": None,
+                "contact": None,
+            },
+        )
+
+        assert_that(
+            item,
+            has_entries(
+                {
+                    "login": "jdoe",
+                    "name": "John Doe",
+                    "contact": contains_exactly("mail1@example.org", "mail2@jdoe.fr"),
+                }
+            ),
+        )
+
+    def test_load_simple_ref(self):
+        """Tests LdapItem.load method for a simple item, without references.
+
+        The item must be loaded from the mocked LDAP response.
+        """
+
+        id_mapper = MapDNToBase64ID()
+        ldap_item = DatabaseItem(
+            id_mapper,
+            {
+                # No reverse is specified in the LdapRef specification, so it is
+                # assumed that the value itself is alread a backo _id. It does
+                # not need to be transformed.
+                "primary_group": LdapRef(attribute="gidNumber")
+            },
+        )
+
+        dn = "uid=jdoe,ou=people,dc=example,dc=org"
+
+        # Real call to the method under test
+        item = ldap_item.load(
+            [
+                {
+                    "dn": dn,
+                    "attributes": {
+                        "gidNumber": 1312,
+                    },
+                }
+            ],
+            {"primary_group": None},
+        )
+
+        assert_that(
+            item,
+            has_entries(
+                {
+                    "primary_group": 1312,
+                }
+            ),
+        )
+
+        assert_that(item.keys(), contains_exactly("primary_group"))
+
+    def test_load_dn_ref(self):
+        """Tests LdapItem.load method for a simple item, without references.
+
+        The item must be loaded from the mocked LDAP response.
+        """
+
+        # Example id mapper used by the LdapItem
+        group_id_mapper = MapAttributeToID("ou=groups,dc=example,dc=org", "gidNumber")
+
+        # Id mapper used by the superadmin reference
+
+        ldap_group_item = DatabaseItem(
+            group_id_mapper,
+            {
+                # Backo field
+                "superadmin": LdapRef(
+                    # LDAP attribute
+                    attribute="owner",
+                    # The owner attribute is a DN that must be converted to a
+                    # valid backo _id
+                    reverse=RefAttributeToBase64ID(),
+                )
+            },
+        )
+
+        dn = "uid=jdoe,ou=people,dc=example,dc=org"
+
+        # Real call to the method under test
+        item = ldap_group_item.load(
+            [
+                {
+                    "dn": dn,
+                    "attributes": {
+                        "owner": "uid=jdoe,ou=people,dc=example,dc=org",
+                    },
+                }
+            ],
+            {"superadmin": None},
+        )
+
+        assert_that(
+            item,
+            has_entries(
+                {
+                    "superadmin": RefAttributeToBase64ID().ref_id(
+                        "uid=jdoe,ou=people,dc=example,dc=org"
+                    ),
+                }
+            ),
+        )